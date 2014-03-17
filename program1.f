c29e5fc754497a6e6c55d630499258747c90f885
ayacob <malemiye@gmail.com>
2014-03-17 16:47:17 +0100

    Create program1.f

---
+++ program1.f
@@ -0,0 +1,21 @@
+
+program LJMD
+
+ implicite none
+ integer  i,a,b,c,n
+ real    sum
+ 
+ sum=0.0
+  n=10
+ 
+ 
+ do i=1, n
+   sum = sum + 1
+   
+  end do
+  
+  print * sum
+  
+end program
+ 
+ 
